4d2784f731993cf82732388631d8551e1d799e82
abhradip-cbnits <abhradip@cbnits.com>
2022-10-17 01:07:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import searchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
-const API_URL = "http://www.omdbapi.com?apikey=eb15a133";
+const API_URL = "https://www.omdbapi.com?apikey=eb15a133";
 
 const App = () => {
 
