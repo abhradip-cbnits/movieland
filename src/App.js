9cf4600857bcc874639294df0325d831a3c4a668
Abhradip Ghosh <aghosh@zuelligpharma.com>
2022-10-16 23:25:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import searchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const API_URL = "http://www.omdbapi.com?apikey=eb15a133";
+
+const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movieList, setMovieList] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data, "--");
+    setMovieList(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('spiderman');
+  },[]);
+
+
+
+  return(
+    <div className='app'>
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+        <input 
+          value={searchTerm}
+          onChange={(e)=> setSearchTerm(e.target.value)} 
+        />
+        
+
+        <img 
+          src={searchIcon}
+          alt="Search"
+          onClick={()=> searchMovies(searchTerm)}
+        />
+      </div>
+
+
+      {
+        movieList?.length > 0 ?
+        (
+          <div className='container'>
+            {movieList.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+          </div>
+        ):
+        (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
     </div>
-  );
+  )
 }
 
 export default App;
